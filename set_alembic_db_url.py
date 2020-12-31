078429968ac7e52b90d1d69045b259a223a11f58
soroushh <soroush@tails.com>
2020-12-31 10:34:22 +0000

    The ConfigParser is added to the alem_requirements.txt.

--- set_alembic_db_url.py
+++ set_alembic_db_url.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         'sqlalchemy.url',
         'postgresql://postgres:Kati8212579!@my-database.cvgmzutcx9w8.eu-west-1.rds.amazonaws.com/postgres'
     )
-    with open('alembic.ini', 'wb') as configfile:
+    with open('alembic.ini', 'w') as configfile:
         parser.write(configfile)
 
 
