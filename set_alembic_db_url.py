b4691b8a3364889020dd00997b24fe17d01a8649
soroushh <soroush@tails.com>
2020-12-31 11:00:01 +0000

    The environment variable is used to refer the database URI.

--- set_alembic_db_url.py
+++ set_alembic_db_url.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser.set(
         'alembic',
         'sqlalchemy.url',
-        'postgresql://postgres:Kati8212579!@my-database.cvgmzutcx9w8.eu-west-1.rds.amazonaws.com/postgres'
+        os.environ['DATABASE_URI']
     )
     with open('alembic.ini', 'w') as configfile:
         parser.write(configfile)
