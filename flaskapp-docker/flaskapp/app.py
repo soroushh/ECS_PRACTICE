25bdd08ddb627e6fb9ba46260408261b98633132
soroushh <soroush@tails.com>
2021-03-26 10:50:38 +0000

    The init_app function is added.

--- flaskapp-docker/flaskapp/app.py
+++ flaskapp-docker/flaskapp/app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, url_for, redirect
-app = Flask(__name__)
+from flask import Flask, render_template
+
+def init_app():
+    """Initialises the application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config['SECRET_KEY'] = 'APP_SECRET_KEY'
+    return app
+
+
+app = init_app()
 
 @app.route('/health')
 def health():
     """An endpoint showing the health of the app."""
-    return 'pong'
+    return render_template('home.html')
 
