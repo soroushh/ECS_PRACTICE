009de75315f755cb0dd991b4f1ca5d3109992800
soroushh <soroush@tails.com>
2021-03-26 10:19:21 +0000

    The extra endpoints are removed.

--- flaskapp-docker/flaskapp/app.py
+++ flaskapp-docker/flaskapp/app.py
@@ -1,27 +1,8 @@
 from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("home.html")
+@app.route('/health')
+def health():
+    """An endpoint showing the health of the app."""
+    return 'pong'
 
-@app.route("/home")
-def home():
-    return redirect(url_for('login'))
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
-@app.route("/hello")
-def hello():
-    return 'hello mate.'
-
-@app.route('/add')
-def test():
-    return 'Endpoint is changed now.'
-
-@app.route('/multiply/<int:first>/<int:second>')
-def multiply(first, second):
-    return f'The multiply of request parameters are {first*second}.'
